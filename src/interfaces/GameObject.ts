1b036c862e327ed03ea864e206aa4fb8664a9f7e
paseri3739 <tael43@icloud.com>
2024-08-17 21:18:57 +0900

    add interface

--- src/interfaces/GameObject.ts
+++ src/interfaces/GameObject.ts
@@ -1,4 +1,9 @@
+import { GameComponent } from "./GameComponent";
+
 export interface GameObject {
     update(deltaTime: number, ...args: any[]): void;
     render(context: CanvasRenderingContext2D): void;
+    processInput(input: any): void;
+    addComponent(component: GameComponent): void;
+    removeComponent(component: GameComponent): void;
 }
